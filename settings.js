e5cbb1ba0440424e41f8dd5dbdd322b96a5a26c3
vimuwamod <102043707+vimuwamod@users.noreply.github.com>
2022-07-01 04:12:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,15 +23,15 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94761062584'] //Owner number in aive msg
+global.premium = ['+94761062584'] //Owner Number info
+global.ownernomer = '+94761062584' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = '༻𝙼.𝚁.ᏣᎽᏰᏋ ᏉᎥᎷ𝐔Ꮙ𝐀༺' //Owner Name
 global.botname = ' ☬RED༒DRAGON' //Bot Name
 
-global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
-global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
+global.button = '🤭YT CHANEL📍' //Costomize A Button Name In Alive Message
+global.btnurl = '🙂🔥WA NUMBER😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
